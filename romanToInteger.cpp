fce893b9fd0d2b263b8c8e44442ad04c98fa5f2a
Noah William Shaffer <67817365+noahwilliamshaffer@users.noreply.github.com>
2020-08-07 14:26:23 -0400

    Update romanToInteger.cpp

--- romanToInteger.cpp
+++ romanToInteger.cpp
@@ -1,5 +1,6 @@
 int romanToInt(string s) 
 {
+    //map to add value to each roman numeral
     unordered_map<char, int> T = { { 'I' , 1 },
                                    { 'V' , 5 },
                                    { 'X' , 10 },
@@ -7,16 +8,20 @@ int romanToInt(string s)
                                    { 'C' , 100 },
                                    { 'D' , 500 },
                                    { 'M' , 1000 } };
-                                   
+    //sum = the value of the last roman numeral in the string                               
    int sum = T[s.back()];
+    //loop backwards through string 
    for (int i = s.length() - 2; i >= 0; --i) 
    {
+       //if the value of previous roman numeral is less than the one in front
        if (T[s[i]] < T[s[i + 1]])
        {
+           //we subtract from sum
            sum -= T[s[i]];
        }
        else
        {
+           //we add to sum
            sum += T[s[i]];
        }
    }
